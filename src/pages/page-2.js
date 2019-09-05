49152b9d87be48ca4ec9c515a4ef119f7e13591f
Eirik Vilpponen <eirik.vilpponen@gmail.com>
2019-09-05 20:07:21 +0200

    edit page 2. add unordered list

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,14 +8,21 @@ const SecondPage = () => <Layout>
     <SEO title="Links and projects"/>
     <h1>Links and projects</h1>
     <p>
-        <span>Check out my </span>
-        <a href="https://github.com/evilppo">GitHub-profile</a>
+        Check me out at these sites:
     </p>
     <p>
-        <span>Check out my </span>
-        <a href="https://platformer-v1.herokuapp.com/">platformer-game</a>
+        <ul>
+        <li>
+            <a href="https://github.com/evilppo">GitHub</a>
+        </li>
+        <li>
+            <a href="https://no.linkedin.com/in/eirik-timo-b%C3%B8e-vilpponen-a2041436?trk=people-guest_profile-result-card_result-card_full-click">LinkedIn</a>
+        </li>
+        </ul>
     </p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>
+        <Link to="/">Go back to the homepage</Link>
+    </p>
 </Layout>
 
 export default SecondPage
