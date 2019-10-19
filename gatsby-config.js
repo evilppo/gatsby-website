cdd14cdcab2e50467373b72d97a67be16e0471dc
Eirik Vilpponen <eirik.vilpponen@gmail.com>
2019-10-19 23:34:05 +0200

    Oppdaterer metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Eirik Vilpponen`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Welcome to my website!`,
+    author: `Eirik Vilpponen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
