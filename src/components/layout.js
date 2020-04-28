d1f0e8d50a0327b7617521031b2d479121cfae20
Eirik Vilpponen <eirik.vilpponen@gmail.com>
2020-04-28 18:45:31 +0200

    linting

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {useStaticQuery, graphql} from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Layout = ({children}) => {
+    const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,52 +23,52 @@ const Layout = ({ children }) => {
     }
   `)
 
-  const xWing =
-    "           __\n" +
-    ".-.__      \\ .-.  ___  __\n" +
-    "|_|  '--.-.-(   \\/\\;;\\_\\.-._______.-.\n" +
-    "(-)___     \\ \\ .-\\ \\;;\\(   \\       \\ \\\n" +
-    " Y    '---._\\_((Q)) \\;;\\\\ .-\\     __(_)\n" +
-    " I           __'-' / .--.((Q))---'    \\,\n" +
-    " I     ___.-:    \\|  |   \\'-'_          \\\n" +
-    " A  .-'      \\ .-.\\   \\   \\ \\ '--.__     '\\\n" +
-    " |  |____.----((Q))\\   \\__|--\\_      \\     '\n" +
-    "    ( )        '-'  \\_  :  \\-' '--.___\\\n" +
-    "     Y                \\  \\  \\       \\(_)\n" +
-    "     I                 \\  \\  \\         \\,\n" +
-    "     I                  \\  \\  \\          \\\n" +
-    "     A                   \\  \\  \\          '\\\n" +
-    "     |                    \\  \\__|           '\n" +
-    "                           \\_:.  \\\n" +
-    "                             \\ \\  \\\n" +
-    "                              \\ \\  \\\n" +
-    "                               \\_\\_|     -PEW PEW!-"
+    const xWing =
+        "           __\n" +
+        ".-.__      \\ .-.  ___  __\n" +
+        "|_|  '--.-.-(   \\/\\;;\\_\\.-._______.-.\n" +
+        "(-)___     \\ \\ .-\\ \\;;\\(   \\       \\ \\\n" +
+        " Y    '---._\\_((Q)) \\;;\\\\ .-\\     __(_)\n" +
+        " I           __'-' / .--.((Q))---'    \\,\n" +
+        " I     ___.-:    \\|  |   \\'-'_          \\\n" +
+        " A  .-'      \\ .-.\\   \\   \\ \\ '--.__     '\\\n" +
+        " |  |____.----((Q))\\   \\__|--\\_      \\     '\n" +
+        "    ( )        '-'  \\_  :  \\-' '--.___\\\n" +
+        "     Y                \\  \\  \\       \\(_)\n" +
+        "     I                 \\  \\  \\         \\,\n" +
+        "     I                  \\  \\  \\          \\\n" +
+        "     A                   \\  \\  \\          '\\\n" +
+        "     |                    \\  \\__|           '\n" +
+        "                           \\_:.  \\\n" +
+        "                             \\ \\  \\\n" +
+        "                              \\ \\  \\\n" +
+        "                               \\_\\_|     -PEW PEW!-"
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      {console.log(xWing)}
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title}/>
+            {console.log(xWing)}
+            <div
+                style={{
+                    margin: `0 auto`,
+                    maxWidth: 960,
+                    padding: `0px 1.0875rem 1.45rem`,
+                    paddingTop: 0,
+                }}
+            >
+                <main>{children}</main>
+                <footer>
+                    © {new Date().getFullYear()}, Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+            </div>
+        </>
+    )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
