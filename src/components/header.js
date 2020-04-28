ff74816b486db572e81372701c45f7ce9c5a9aa2
Eirik Vilpponen <eirik.vilpponen@gmail.com>
2020-04-28 17:54:36 +0200

    Change header color to dark slate gray

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `darkolivegreen`,
+      background: `darkslategray`,
       marginBottom: `1.45rem`,
     }}
   >
